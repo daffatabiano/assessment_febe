11931996aebc7617d98c2611601ccafb6f03955b
unknown <daffatabianoo@gmail.com>
2024-09-14 02:47:58 +0700

    fix(headers): toggle fixing checked value

--- src/components/headers/index.tsx
+++ src/components/headers/index.tsx
@@ -94,6 +94,7 @@ export default function Navbar() {
               type="checkbox"
               onClick={toggleDarkMode}
               value=""
+              checked={darkMode}
               className="sr-only peer"
             />
             <div className="relative w-11 h-6 bg-zinc-100 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-zinc-300 dark:peer-focus:ring-zinc-100 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-zinc-600"></div>
