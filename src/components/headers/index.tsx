8846b661986efdf26c9afb0c57f48cf1b44fb766
unknown <daffatabianoo@gmail.com>
2024-09-13 21:29:45 +0700

    feat(headers): navbar toggle darkmode

--- src/components/headers/index.tsx
+++ src/components/headers/index.tsx
@@ -9,8 +9,26 @@ export default function Navbar() {
   const [scrollY, setScrollY] = useState<boolean>(false);
   const [menu, setMenu] = useState<boolean>(false);
   const { asPath } = useRouter();
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+  const [theme, setTheme] = useState<string>('');
 
-  console.log(asPath);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setTheme(window.localStorage.getItem('theme') || 'light');
+    }
+    if (theme) {
+      setDarkMode(theme === 'dark');
+      document.body.classList.toggle('dark', theme === 'dark');
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    if (typeof window !== 'undefined') {
+      window.localStorage.setItem('theme', darkMode ? 'light' : 'dark');
+    }
+    document.body.classList.toggle('dark');
+  };
 
   useEffect(() => {
     const scrollY = window.scrollY;
@@ -67,8 +85,13 @@ export default function Navbar() {
             <span className="me-3 text-sm font-medium text-gray-900 dark:text-gray-300">
               <FaRegSun />
             </span>
-            <input type="checkbox" value="" className="sr-only peer" />
-            <div className="relative w-11 h-6 bg-yellow-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-zinc-300 dark:peer-focus:ring-zinc-100 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-zinc-600"></div>
+            <input
+              type="checkbox"
+              onClick={toggleDarkMode}
+              value=""
+              className="sr-only peer"
+            />
+            <div className="relative w-11 h-6 bg-zinc-100 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-zinc-300 dark:peer-focus:ring-zinc-100 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-zinc-600"></div>
             <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
               <FaRegMoon />
             </span>
