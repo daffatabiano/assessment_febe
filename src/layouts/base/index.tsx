1aa1e712f88eec3cbc54feee673a8c1e79e1a328
unknown <daffatabianoo@gmail.com>
2024-09-13 21:42:45 +0700

    feat(layouts): set darkmode toggle

--- src/layouts/base/index.tsx
+++ src/layouts/base/index.tsx
@@ -6,9 +6,9 @@ export default function BaseLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <div className="min-h-screen bg-rose-400 w-full">
       <Navbar />
       {children}
-    </>
+    </div>
   );
 }
