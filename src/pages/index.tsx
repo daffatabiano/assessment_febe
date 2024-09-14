47d91ab69e7c09317f3dd8462c18052d44968ef3
unknown <daffatabianoo@gmail.com>
2024-09-14 11:21:05 +0700

    add(packages): axios for fetching data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { Hero } from '@/components/Hero';
+import { Service } from '@/components/Service';
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Service />
     </>
   );
 }
