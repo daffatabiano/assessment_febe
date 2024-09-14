b524861eb8e4ed9bf760213264ecee6c1928f6fb
unknown <daffatabianoo@gmail.com>
2024-09-14 13:04:41 +0700

    feat(component): service components fetching and slicing for pagination preparation"

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -1,11 +1,30 @@
+import { useState } from 'react';
 import { FaFilter } from 'react-icons/fa';
 
-const Service = () => {
+const Service = ({
+  data,
+}: {
+  data: {
+    id: number;
+    title: string;
+    body: string;
+    userId: number;
+  }[];
+}) => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const postsPerPage = 5;
+  const totalPages = Math.ceil(data?.length / postsPerPage);
+
+  const currentPosts = data.slice(
+    (currentPage - 1) * postsPerPage,
+    currentPage * postsPerPage
+  );
+
   return (
-    <div id="service" className="w-full flex px-8 pt-24">
+    <div id="service" className="w-full flex-col flex px-8 pt-24">
       <div className="w-full flex justify-between">
         <h1 className="text-3xl font-bold text-zinc-950 dark:text-white">
-          List Service
+          Blog Service
         </h1>
         <button className="bg-transparent rounded-full text-zinc-950 dark:text-white flex w-28   justify-center items-center py-2 border-2 border-zinc-950 dark:border-white">
           <span className="mr-2">
@@ -13,6 +32,18 @@ const Service = () => {
           </span>{' '}
           Filter
         </button>
+      </div>
+      <div className="  m-2 mt-8">
+        <ul className="w-full flex justify-center items-center flex-col gap-4 md:gap-4">
+          {currentPosts?.map((item, i) => (
+            <li
+              key={i}
+              className="text-zinc-950 dark:text-white md:w-1/2 p-4 flex flex-col gap-2 rounded-lg bg-zinc-200 dark:bg-zinc-800 shadow-sm shadow-zinc-950 dark:shadow-white hover:transform hover:scale-105 md:hover:translate-x-6">
+              <h1 className="font-bold text-lg capitalize">{item?.title}</h1>
+              <p className="text-sm font-light">{item.body}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import { Hero } from '@/components/Hero';
 import { Service } from '@/components/Service';
+import axios from 'axios';
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <>
       <Hero />
-      <Service />
+      <Service data={data} />
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  const data = res.data;
+  return { props: { data } };
 }
