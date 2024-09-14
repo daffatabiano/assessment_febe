f82a70ec65a34744d0ce9f21093e6aa975d4946c
unknown <daffatabianoo@gmail.com>
2024-09-14 10:28:27 +0700

    feat(hero):responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,18 @@
 const Hero = () => {
   return (
-    <div className="hero min-h-screen bg-[url('/light-bg.jpeg')] bg-cover bg-center dark:bg-[url('/dark-bg.jpeg')] bg-no-repeat">
-      <div className="flex-col lg:flex-row-reverse">
-        <div className="max-w-md">
+    <div className="hero h-screen dark:shadow-[inset_0_-34px_65px_40px_rgba(10,10,10,1)] shadow-[inset_0_-34px_65px_40px_rgba(255,255,255,1)] bg-[url('/light-bg.jpeg')] bg-cover bg-center dark:bg-[url('/dark-bg.jpeg')] bg-no-repeat">
+      <div className="flex-col-reverse flex md:justify-between justify-center gap-8 items-center md:flex-row px-8 pt-20">
+        <div className="max-w-md text-zinc-950 dark:text-white text-center md:text-left">
           <h1 className="text-5xl font-bold">Next.JS Assessment FEBE</h1>
-          <p className="text-2xl py-6 ">PT. Arnawa Teknologi Informasi</p>
+          <p className="text-2xl py-6 font-medium text-balance">
+            PT. Arnawa Teknologi Informasi
+          </p>
           <button className="bg-primary dark:bg-secondary text-white p-4 rounded w-14">
             Read More
           </button>
         </div>
-        <div className="max-w-md">
-          <img
-            src="/arnawa.png"
-            alt="company-logo"
-            className="w-full h-full object-cover object-center"
-          />
+        <div className=" w-1/2 flex justify-center">
+          <img src="/arnawa.png" alt="company-logo" className="w-48 h-48" />
         </div>
       </div>
     </div>
