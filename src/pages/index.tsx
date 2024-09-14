474d41d4ea514042f85e5b01635711dfe3b30b8e
unknown <daffatabianoo@gmail.com>
2024-09-14 10:00:39 +0700

    feat(components): Hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import { Hero } from '@/components/Hero';
+
 export default function Home() {
   return (
     <>
-      <div className="text-3xl text-rose-400 text-balance"> HELLOW RWOEWL!</div>
+      <Hero />
     </>
   );
 }
