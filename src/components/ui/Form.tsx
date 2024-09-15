f95d1ca57bfc737ea1e07f91b0d3353f12eacfaf
unknown <daffatabianoo@gmail.com>
2024-09-15 19:25:05 +0700

    opt(ui): form ui optimizations and accessibility improvements

--- src/components/ui/Form.tsx
+++ src/components/ui/Form.tsx
@@ -1,4 +1,4 @@
-import { FormEventHandler } from 'react';
+import { FormEventHandler, useCallback } from 'react';
 
 type formProps = {
   onSubmit: FormEventHandler<HTMLFormElement>;
@@ -29,9 +29,12 @@ const Label = ({ text }: { text: string }) => {
 
 const Form = (props: formProps) => {
   const { onSubmit } = props;
+  const handleSubmit = useCallback(() => {
+    onSubmit;
+  }, [onSubmit]);
   return (
     <div className="w-[90%] md:w-1/3 sm:m-8 m-4 p-4 rounded-lg bg-zinc-100 dark:bg-zinc-800 shadow-md dark:shadow-zinc-900">
-      <form className="flex flex-col gap-4" onSubmit={onSubmit}>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <Label text="Name" />
         <Input type="text" placeholder="Input your name here" name="name" />
         <Label text="Email" />
@@ -41,9 +44,11 @@ const Form = (props: formProps) => {
           className="w-full h-32 p-2 rounded-xl text-zinc-950 dark:text-zinc-50 bg-zinc-200 dark:bg-zinc-700 focus:outline-none resize-none"
           placeholder="Input your message here"
           name="message"
+          aria-label="Input your message here"
         />
         <button
           type="submit"
+          aria-label="Send message"
           className="w-full bg-zinc-950 text-white dark:text-zinc-950 text-lg mt-4 dark:bg-white p-2 rounded-xl">
           Send
         </button>
