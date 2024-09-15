53921b8c248a5195967452643cde858d7998f079
unknown <daffatabianoo@gmail.com>
2024-09-15 14:58:12 +0700

    fix(contact): size contact title fixing

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -60,7 +60,7 @@ const Page = () => {
   return (
     <div className="w-full h-screen flex justify-center items-center flex-col mt-8">
       <Toaster />
-      <h1 className="text-5xl font-extrabold text-balance text-center text-zinc-300 dark:text-zinc-900">
+      <h1 className="text-3xl md:text-5xl font-extrabold text-balance text-center text-zinc-300 dark:text-zinc-900">
         Contact.
       </h1>
       <Form onSubmit={submitForm} />
