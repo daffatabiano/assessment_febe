3927685ccbbec44bfd5cf9f915ad9b3d00e740c8
unknown <daffatabianoo@gmail.com>
2024-09-15 19:20:54 +0700

    opt(ui): dropdown optimizations and accessibility improvements

--- src/components/ui/Dropdown.tsx
+++ src/components/ui/Dropdown.tsx
@@ -1,3 +1,5 @@
+import { useCallback } from 'react';
+
 const styles = {
   button:
     'block px-4 w-full text-start rounded-lg py-2 hover:bg-zinc-100 dark:hover:bg-zinc-600 dark:hover:text-white',
@@ -10,8 +12,15 @@ export default function Dropdown({
   open: boolean;
   filter: () => void;
 }) {
+  const handleFilter = useCallback(() => {
+    filter();
+  }, [filter]);
+
   return (
     <div
+      role="menu"
+      aria-hidden={!open}
+      aria-labelledby="dropdownDefaultButton"
       className={`z-10 ${
         open ? 'block' : 'hidden'
       } absolute right-0 top-full mt-4 bg-white divide-y divide-zinc-100 rounded-lg shadow w-44 dark:bg-zinc-700`}>
@@ -19,18 +28,30 @@ export default function Dropdown({
         className="p-2 text-sm text-zinc-700 dark:text-zinc-200"
         aria-labelledby="dropdownDefaultButton">
         <li>
-          <button type="button" onClick={filter} className={styles.button}>
+          <button
+            type="button"
+            onClick={handleFilter}
+            className={styles.button}
+            role="menuitem">
             Short Title
           </button>
         </li>
         <li>
-          <button type="button" onClick={filter} className={styles.button}>
+          <button
+            aria-label="menuitem"
+            type="button"
+            onClick={filter}
+            className={styles.button}>
             Long Title
           </button>
         </li>
         <hr className="h-px my-3 bg-zinc-200 border-1 mx-4 dark:bg-zinc-700" />
         <li>
-          <button type="button" onClick={filter} className={styles.button}>
+          <button
+            role="menuitem"
+            type="button"
+            onClick={filter}
+            className={styles.button}>
             Default
           </button>
         </li>
