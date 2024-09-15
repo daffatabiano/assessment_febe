c5d1bd81959def46d647d7e85d31c5755a41432a
unknown <daffatabianoo@gmail.com>
2024-09-15 14:40:39 +0700

    feat(ui): drawer active path

--- src/components/headers/index.tsx
+++ src/components/headers/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { navLinks } from '@/constants';
 import { FaRegMoon, FaRegSun } from 'react-icons/fa';
+import Drawer from '../ui/Drawer';
 
 export default function Navbar() {
   const [scrollY, setScrollY] = useState<boolean>(false);
@@ -49,65 +50,89 @@ export default function Navbar() {
   }, []);
 
   return (
-    <div
-      className={`w-full md:h-24 h-16 flex fixed top-0 z-10 p-4  ${
-        scrollY ? 'bg-white dark:bg-zinc-950' : 'bg-transparent'
-      }`}>
-      <div className="w-full px-2 flex justify-between items-center">
-        <div className="w-full md:w-1/2 flex items-center">
-          <img
-            src={`${theme ? '/dark-logo.png' : '/logo.png'}`}
-            alt="logo-navbar"
-            className="md:w-16 md:h-16 w-10 h-10 object-cover"
-          />
-          <h1 className="text-md font-semibold ms-2 md:text-2xl flex w-fullfont-bold text-zinc-700 dark:text-white">
-            Daffa Tabiano{' '}
-            <span className="ps-2 text-zinc-400 dark:text-zinc-300">
-              | FE Developer
-            </span>{' '}
-          </h1>
-        </div>
-        <div className="md:w-1/2 w-fit flex items-center justify-end ">
-          <ul className="gap-4 hidden md:flex">
-            {navLinks.map((item) => (
-              <li
-                key={item.id}
-                className={` text-balance hover:text-zinc-900 dark:hover:text-zinc-100 hover:-translate-y-1 ${
-                  item.path === asPath
-                    ? 'text-zinc-900 dark:text-zinc-100 font-bold -translate-y-1'
-                    : 'text-zinc-700 dark:text-zinc-300 font-light'
-                }`}>
-                <Link href={asPath === '/contact' ? '/' : item.path}>
+    <>
+      <Drawer setIsOpen={setMenu} isOpen={menu}>
+        <ul className="w-full h-full flex flex-col items-center p-4 gap-4">
+          {navLinks.map((item) => {
+            return (
+              <li key={item.id} className="w-full flex flex-col ">
+                <Link
+                  className={`relative text-lg font-medium text-zinc-950 dark:text-white p-2 rounded-lg hover:bg-zinc-200 dark:hover:bg-zinc-700 ${
+                    asPath === item.path
+                      ? 'text-zinc-500 dark:text-zinc-300 bg-zinc-200 dark:bg-zinc-700 '
+                      : ''
+                  }`}
+                  href={`${item.path}`}>
                   {item.title}
+                  {asPath === item.path && (
+                    <span className="absolute my-auto top-0 bottom-0 right-4 bg-zinc-500 dark:bg-zinc-300 h-2 w-2 rounded-full" />
+                  )}
                 </Link>
               </li>
-            ))}
-          </ul>
-          <button
-            type="button"
-            onClick={() => setMenu(!menu)}
-            className="bg-transparent w-8 h-8 md:hidden text-xl dark:text-white">
-            <GiHamburgerMenu />
-          </button>
-
-          <label className="md:inline-flex hidden items-center cursor-pointer ps-4">
-            <span className="me-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-              <FaRegSun />
-            </span>
-            <input
-              type="checkbox"
-              onClick={toggleDarkMode}
-              value=""
-              checked={darkMode}
-              className="sr-only peer"
+            );
+          })}
+        </ul>
+      </Drawer>
+      <div
+        className={`w-full md:h-24 h-16 flex fixed top-0 z-10 p-4  ${
+          scrollY ? 'bg-white dark:bg-zinc-950' : 'bg-transparent'
+        }`}>
+        <div className="w-full px-2 flex justify-between items-center">
+          <div className="w-full md:w-1/2 flex items-center">
+            <img
+              src={`${theme ? '/dark-logo.png' : '/logo.png'}`}
+              alt="logo-navbar"
+              className="md:w-16 md:h-16 w-10 h-10 object-cover"
             />
-            <div className="relative w-11 h-6 bg-zinc-100 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-zinc-300 dark:peer-focus:ring-zinc-100 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-zinc-600"></div>
-            <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-              <FaRegMoon />
-            </span>
-          </label>
+            <h1 className="text-md font-semibold ms-2 md:text-2xl flex w-fullfont-bold text-zinc-700 dark:text-white">
+              Daffa Tabiano{' '}
+              <span className="ps-2 text-zinc-400 dark:text-zinc-300">
+                | FE Developer
+              </span>{' '}
+            </h1>
+          </div>
+          <div className="md:w-1/2 w-fit flex items-center justify-end ">
+            <ul className="gap-4 hidden md:flex">
+              {navLinks.map((item) => (
+                <li
+                  key={item.id}
+                  className={` text-balance hover:text-zinc-900 dark:hover:text-zinc-100 hover:-translate-y-1 ${
+                    item.path === asPath
+                      ? 'text-zinc-900 dark:text-zinc-100 font-bold -translate-y-1'
+                      : 'text-zinc-700 dark:text-zinc-300 font-light'
+                  }`}>
+                  <Link href={asPath === '/contact' ? '/' : item.path}>
+                    {item.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <button
+              type="button"
+              onClick={() => setMenu(!menu)}
+              className="bg-transparent w-8 h-8 md:hidden text-xl dark:text-white">
+              <GiHamburgerMenu />
+            </button>
+
+            <label className="md:inline-flex hidden items-center cursor-pointer ps-4">
+              <span className="me-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+                <FaRegSun />
+              </span>
+              <input
+                type="checkbox"
+                onClick={toggleDarkMode}
+                value=""
+                checked={darkMode}
+                className="sr-only peer"
+              />
+              <div className="relative w-11 h-6 bg-zinc-100 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-zinc-300 dark:peer-focus:ring-zinc-100 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-zinc-600"></div>
+              <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+                <FaRegMoon />
+              </span>
+            </label>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/components/ui/Drawer.tsx
+++ src/components/ui/Drawer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaArrowAltCircleRight } from 'react-icons/fa';
 
 export default function Drawer({
   children,
@@ -8,18 +9,23 @@ export default function Drawer({
   return (
     <main
       className={
-        ' fixed overflow-hidden z-10 bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out ' +
+        ' fixed overflow-hidden md:hidden z-[999] bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out ' +
         (isOpen
           ? ' transition-opacity opacity-100 duration-500 translate-x-0  '
           : ' transition-all delay-500 opacity-0 translate-x-full  ')
       }>
       <section
         className={
-          ' w-screen max-w-lg right-0 absolute bg-white h-full shadow-xl delay-400 duration-500 ease-in-out transition-all transform  ' +
+          ' w-screen max-w-lg right-0 absolute bg-white dark:bg-zinc-800 h-full shadow-xl delay-400 duration-500 ease-in-out transition-all transform  ' +
           (isOpen ? ' translate-x-0 ' : ' translate-x-full ')
         }>
         <article className="relative w-screen max-w-lg pb-10 flex flex-col space-y-6 overflow-y-scroll h-full">
-          <header className="p-4 font-bold text-lg">Header</header>
+          <header className="p-4 font-bold text-lg flex justify-between text-zinc-800 dark:text-zinc-50">
+            Header{' '}
+            <span onClick={() => setIsOpen(false)}>
+              <FaArrowAltCircleRight className="text-xl" />
+            </span>
+          </header>
           {children}
         </article>
       </section>
